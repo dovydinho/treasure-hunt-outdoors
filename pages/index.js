25b24b4f743e8157aa082a7706c2771220eda12d
dovydinho <dovydas.lapinskas@tutanota.com>
2022-08-20 22:54:54 +0300

    responsive design update

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,63 @@ export default function Home() {
 
   return (
     <>
-      <section className="xl:container pt-12 lg:flex h-[83vh]">
-        <div className="lg:w-1/3 rounded-lg flex flex-col px-3 md:pb-12 max-h-[38rem] md:max-h-[53rem] xl:mr-3 lg:mr-2">
+      <section className="container flex flex-col-reverse sm:flex-row gap-6 p-2 lg:pt-12 text-white">
+        <div className="sm:w-1/2 md:w-1/3">
+          {treasureInitContracts.length > 0 && (
+            <input
+              className="w-full
+                    rounded-full bg-transparent 
+                    border border-dashed border-indigo-600 
+                    mb-5 px-8 py-2 focus:outline-none 
+                    placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
+              placeholder="Search by treasure title or address..."
+              onKeyUp={(e) => search(e.target.value)}
+            />
+          )}
+          <div className="flex flex-col h-[65vh] lg:h-[74vh]">
+            <TreasureList treasures={treasureContracts} filters={filters} />
+          </div>
+        </div>
+        <div className="w-full sm:w-1/2 md:w-2/3 h-96 min-h-0 sm:h-[80vh] lg:h-[81vh]">
+          <Map
+            className="min-h-full rounded-lg z-0"
+            center={position}
+            zoom={14}
+          >
+            {({ TileLayer, Marker, Popup, useMap }, L, MarkerClusterGroup) => (
+              <>
+                <MapEffect useMap={useMap} />
+
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                />
+
+                <MarkerClusterGroup>
+                  {treasureContracts.map(function (treasure, i) {
+                    return (
+                      <Marker position={[treasure[5], treasure[6]]} key={i}>
+                        <Popup className="custom-popup-style">
+                          <span class="text-xs inline-block py-1 px-2.5 leading-none text-center whitespace-nowrap align-baseline font-bold bg-green-500 text-white rounded-full">
+                            {treasure[0] &&
+                              treasure[0].slice(2, 6) +
+                                `-` +
+                                treasure[0].slice(38, 42)}
+                          </span>
+                          <p className="text-sm font-bold">{treasure[2]}</p>
+                        </Popup>
+                      </Marker>
+                    );
+                  })}
+                </MarkerClusterGroup>
+              </>
+            )}
+          </Map>
+        </div>
+      </section>
+
+      {/* <section className="flex lg:container pr-2 lg:pr-0 pt-4 lg:pt-12 sm:h-[83vh]">
+        <div className="flex flex-col w-full lg:w-1/3 rounded-lg px-3 md:pb-12 max-h-[44rem] md:max-h-[53rem] xl:mr-3 lg:mr-2">
           {treasureInitContracts.length > 0 && (
             <input
               className="
@@ -58,7 +113,7 @@ export default function Home() {
           )}
           <TreasureList treasures={treasureContracts} filters={filters} />
         </div>
-        <div className="lg:w-2/3 md:w-full relative pb-6 relative">
+        <div className="lg:w-2/3 w-full relative pb-6">
           <Map
             className="w-full min-h-full rounded-lg z-0"
             center={position}
@@ -94,7 +149,7 @@ export default function Home() {
             )}
           </Map>
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
