c887098a214a36f14154f93ff7831afa2d62bff9
dovydinho <dovydas.lapinskas@tutanota.com>
2022-09-09 20:58:29 +0300

    Handling form submition

--- pages/hide.js
+++ pages/hide.js
@@ -49,6 +49,11 @@ export default function Hide() {
     }
   };
 
+  const onSubmit = (e) => {
+    e.preventDefault();
+    createTreasure();
+  };
+
   const MapEffect = ({ useMap }) => {
     let data = useMap();
     setTimeout(() => setMap(data), 0);
@@ -72,88 +77,92 @@ export default function Hide() {
   return (
     <>
       <div className="container max-w-[60rem] px-4 md:px-24 py-8 md:py-12 lg:py-24 text-gray-100">
-        <div className="inline-flex gap-3">
-          <ViewGridAddIcon className="w-8 h-8" />
-          <p className="font-bold text-xl md:text-2xl text-violet-600">
-            Hide a Cache
-          </p>
-        </div>
-
-        <div className="mt-4 sm:mt-6 lg:mt-12 grid grid-cols-3 gap-4">
-          <div className="text-violet-600 font-medium">Title</div>
-          <div className="col-span-3 md:col-span-2">
-            <input
-              className="w-full rounded-xl bg-transparent border border-dashed border-indigo-600 mb-5 px-8 py-2 focus:outline-none placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
-              placeholder="Give cache a name..."
-              name="title"
-              onChange={handleTitleChange}
-            />
-          </div>
-          <div className="text-violet-600 font-medium">Hint</div>
-          <div className="col-span-3 md:col-span-2">
-            <textarea
-              className="w-full rounded-xl bg-transparent border border-dashed border-indigo-600 mb-5 px-8 py-2 focus:outline-none placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
-              placeholder="Hidden in a van down by the river..."
-              name="hint"
-              onChange={handleHintChange}
-            />
+        <form onSubmit={(e) => onSubmit(e)}>
+          <div className="inline-flex gap-3">
+            <ViewGridAddIcon className="w-8 h-8" />
+            <p className="font-bold text-xl md:text-2xl text-violet-600">
+              Hide a Cache
+            </p>
           </div>
 
-          <div className="col-span-3">
-            <div className="mt-4 sm:mt-6 lg:mt-12 mb-12 w-full h-[20rem]">
-              <div className="inline-flex">
-                <LocationMarkerIcon className="w-7 h-7 mt-4 text-violet-600" />
-                <p className="ml-2 my-4 text-violet-600 text-xl font-medium">
-                  Mark it on the Map
-                </p>
-              </div>
-              <Map
-                className="w-full min-h-full rounded-lg"
-                center={DEFAULT_CENTER}
-                zoom={13}
-              >
-                {({ TileLayer, Marker, useMap }) => (
-                  <>
-                    <MapEffect useMap={useMap} />
-
-                    <TileLayer
-                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    />
-
-                    <Marker position={newLocation} />
-                  </>
-                )}
-              </Map>
+          <div className="mt-4 sm:mt-6 lg:mt-12 grid grid-cols-3 gap-4">
+            <div className="text-violet-600 font-medium">Title</div>
+            <div className="col-span-3 md:col-span-2">
+              <input
+                required
+                className="w-full rounded-xl bg-transparent border border-dashed border-indigo-600 mb-5 px-8 py-2 focus:outline-none placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
+                placeholder="Give cache a name..."
+                name="title"
+                onChange={handleTitleChange}
+              />
             </div>
-          </div>
-          <div className="my-4 text-violet-600 font-medium">Lat and Long</div>
-          <div className="my-4 col-span-2 text-gray-500">
-            {newLocation[0]}, {newLocation[1]}
-          </div>
+            <div className="text-violet-600 font-medium">Hint</div>
+            <div className="col-span-3 md:col-span-2">
+              <textarea
+                required
+                className="w-full rounded-xl bg-transparent border border-dashed border-indigo-600 mb-5 px-8 py-2 focus:outline-none placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
+                placeholder="Hidden in a van down by the river..."
+                name="hint"
+                onChange={handleHintChange}
+              />
+            </div>
 
-          <div className="sm:my-6">
-            <Button
-              onClick={createTreasure}
-              className="
+            <div className="col-span-3">
+              <div className="mt-4 sm:mt-6 lg:mt-12 mb-12 w-full h-[20rem]">
+                <div className="inline-flex">
+                  <LocationMarkerIcon className="w-7 h-7 mt-4 text-violet-600" />
+                  <p className="ml-2 my-4 text-violet-600 text-xl font-medium">
+                    Mark it on the Map
+                  </p>
+                </div>
+                <Map
+                  className="w-full min-h-full rounded-lg"
+                  center={DEFAULT_CENTER}
+                  zoom={13}
+                >
+                  {({ TileLayer, Marker, useMap }) => (
+                    <>
+                      <MapEffect useMap={useMap} />
+
+                      <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                      />
+
+                      <Marker position={newLocation} />
+                    </>
+                  )}
+                </Map>
+              </div>
+            </div>
+            <div className="my-4 text-violet-600 font-medium">Lat and Long</div>
+            <div className="my-4 col-span-2 text-gray-500">
+              {newLocation[0]}, {newLocation[1]}
+            </div>
+
+            <div className="sm:my-6">
+              <Button
+                type="submit"
+                className="
                             rounded-full border border-indigo-600
                             text-indigo-500 hover:text-gray-100
                             px-8 py-2 inline-flex
                             hover:bg-indigo-500"
-            >
-              {buttonLoading == false ? (
-                <>
-                  Create <EmojiHappyIcon className="ml-1 w-6 h-6" />
-                </>
-              ) : (
-                <>
-                  <div className="animate-spin w-6 h-6 border-b-2 border-gray-100 rounded-full mr-2" />{' '}
-                  Creating...
-                </>
-              )}
-            </Button>
+              >
+                {buttonLoading == false ? (
+                  <>
+                    Create <EmojiHappyIcon className="ml-1 w-6 h-6" />
+                  </>
+                ) : (
+                  <>
+                    <div className="animate-spin w-6 h-6 border-b-2 border-gray-100 rounded-full mr-2" />{' '}
+                    Creating...
+                  </>
+                )}
+              </Button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </>
   );
