b845555985a731de8b8a873abd70130c6f6dcf57
dovydinho <dovydas.lapinskas@tutanota.com>
2022-09-21 20:35:24 +0300

    Footer update

--- components/ui/common/index.js
+++ components/ui/common/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from './navbar';
 export { default as Button } from './button';
+export { default as Footer } from './footer';
 export { default as Map } from './map';
 export { default as Search } from './search';
