9bd7d09a739bb8b67d4e0b00c39b322d88b16361
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-07 17:15:06 +0200

    Google Analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,23 @@ function MyApp({ Component, pageProps }) {
   return (
     <Web3Provider>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `
+          }}
+        />
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
