fde66bc8b572d4a27b9349c755c83de512bcf35e
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-11 19:30:01 +0200

    Update search field style

--- components/ui/common/search/index.js
+++ components/ui/common/search/index.js
@@ -4,8 +4,10 @@ export default function Search({ search }) {
       className="w-full
               rounded-full bg-transparent 
               border border-dashed border-indigo-600 
-              mb-5 px-8 py-2 focus:outline-none 
-              placeholder:text-sm placeholder:text-indigo-600 text-gray-100"
+              mb-5 px-8 py-2 focus:outline-none
+              placeholder:text-sm placeholder:text-indigo-600 text-gray-100
+              hover:border-solid hover:cursor-pointer
+              focus:border-solid focus:border-indigo-600 focus:shadow focus:shadow-white/10"
       placeholder="Search by treasure title or address..."
       onKeyUp={(e) => search(e.target.value)}
     />
